8da6df2682c148b30e40f681beb97cd0eada8fb5
Charles-Isaac Côté <charl_i_c@hotmail.com>
2016-11-18 16:42:20 -0500

    Begin Snake struct

---
+++ Snake.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "stdafx.h"
+struct	Snake
+{
+	POINT* m_Position;
+	int m_IdexTete;
+	int m_Vitesse;
+
+
+	Snake()
+	{
+		m_Position = new POINT[5]{ 0 };
+		m_IdexTete = 0;
+		m_Vitesse = 100;
+	}
+	Snake(int Vitesse) :Snake()
+	{
+		m_Vitesse = Vitesse;
+	}
+	Snake(int Vitesse, int Longueur)
+	{
+		m_Position = new POINT[Longueur]{ 0 };
+		m_IdexTete = 0;
+		m_Vitesse = Vitesse;
+	}
+};
