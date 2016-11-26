4897a2a51567f5194daa7026c26f5d51c7d557b7
JayLabrie <jeromelab@hotmail.fr>
2016-11-26 09:34:55 -0500

    Add files via upload

---
+++ Snake.cpp
@@ -0,0 +1,127 @@
+
+#include "stdafx.h"
+#include "Snake.h"
+/*
+int Snake::LongueurDuSerpent()
+{
+return sizeof(m_Position) / sizeof(Point);
+}*/
+void Snake::Allonger(int Longueur = 1)
+{
+
+
+
+	Point *NewSnake = new Point[m_LongueurDuSerpentCourant + Longueur];
+
+	NewSnake[0] = m_Position[m_IndexTete];
+	Point* p = m_Position;
+	for (int i = 0; i < Longueur; i++)
+	{
+		NewSnake[i + 1] = m_Position[(m_IndexTete + 1) % m_LongueurDuSerpentCourant];
+	}
+	m_Position = p;
+	for (int i = 1; i < m_LongueurDuSerpentCourant; i++)
+	{
+		NewSnake[i + Longueur] = m_Position[(m_IndexTete + i) % m_LongueurDuSerpentCourant];
+
+	}
+
+	m_LongueurDuSerpentCourant += Longueur;
+
+	m_IndexTete = 0;
+	delete[] m_Position;
+
+	m_Position = NewSnake;
+	//delete[] NewSnake;
+}
+
+bool Snake::Update(char DirectionDuDeplacement, Point WindowSize)
+{
+	//a = gauche, w = haut , d = droit , s = bas
+
+	m_Direction = DirectionDuDeplacement;
+
+
+
+	*(m_Position + ((m_IndexTete + 1) % m_LongueurDuSerpentCourant)) = *(m_Position + m_IndexTete);
+	m_IndexTete = (m_IndexTete + 1) % m_LongueurDuSerpentCourant;
+
+
+	switch (m_Direction)
+	{
+	case 'W':
+		(*(m_Position + m_IndexTete)).Y -= m_Vitesse;
+		break;
+	case 'A':
+		(*(m_Position + m_IndexTete)).X -= m_Vitesse;
+		break;
+	case 'S':
+		(*(m_Position + m_IndexTete)).Y += m_Vitesse;
+		break;
+	case 'D':
+		(*(m_Position + m_IndexTete)).X += m_Vitesse;
+		break;
+	default:
+		(*(m_Position + m_IndexTete)).X = 0;
+		(*(m_Position + m_IndexTete)).Y = 0;
+		break;
+	}
+
+
+	if ((*(m_Position + m_IndexTete)).Y > WindowSize.Y || (*(m_Position + m_IndexTete)).Y < 0 || (*(m_Position + m_IndexTete)).X > WindowSize.X || (*(m_Position + m_IndexTete)).X < 0)
+	{
+		return true;
+	}
+	for (auto i = 0; i < m_LongueurDuSerpentCourant; i++)
+	{
+		if ((*(m_Position + m_IndexTete)).Y == (*(m_Position + i)).Y && (*(m_Position + m_IndexTete)).X == (*(m_Position + i)).X && i != m_IndexTete)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+bool Snake::Update(Point WindowSize)
+{
+	//a = gauche, w = haut , d = droit , s = bas
+
+
+
+	*(m_Position + ((m_IndexTete + 1) % m_LongueurDuSerpentCourant)) = *(m_Position + m_IndexTete);
+	m_IndexTete = (m_IndexTete + 1) % m_LongueurDuSerpentCourant;
+
+
+	switch (m_Direction)
+	{
+	case 'W':
+		(*(m_Position + m_IndexTete)).Y -= m_Vitesse;
+		break;
+	case 'A':
+		(*(m_Position + m_IndexTete)).X-= m_Vitesse;
+		break;
+	case 'S':
+		(*(m_Position + m_IndexTete)).Y += m_Vitesse;
+		break;
+	case 'D':
+		(*(m_Position + m_IndexTete)).X+= m_Vitesse;
+		break;
+	default:
+		(*(m_Position + m_IndexTete)).X = 0;
+		(*(m_Position + m_IndexTete)).Y = 0;
+		break;
+	}
+
+
+	if ((*(m_Position + m_IndexTete)).Y > WindowSize.Y || (*(m_Position + m_IndexTete)).Y < 0 || (*(m_Position + m_IndexTete)).X > WindowSize.X || (*(m_Position + m_IndexTete)).X < 0)
+	{
+		return true;
+	}
+	for (auto i = 0; i < m_LongueurDuSerpentCourant; i++)
+	{
+		if ((*(m_Position + m_IndexTete)).Y == (*(m_Position + i)).Y && (*(m_Position + m_IndexTete)).X == (*(m_Position + i)).X && i != m_IndexTete)
+		{
+			return true;
+		}
+	}
+	return false;
+}
--- Snake.h
+++ Snake.h
@@ -1,29 +1,49 @@
 #pragma once
-#include "stdafx.h"
-struct	Snake
+#include "Types.h"
+class Snake
 {
-	POINT* m_Position;
-	int m_IdexTete;
+	//ICI vont les variable privés:
+	//Oui, elle sont tous publiques pour le moment
+
+
+public: //Met TOUTES le variables membres publiques
+	Point *m_Position; //Tableau circulaire des segments du corp
+	int m_IndexTete;
 	int m_Vitesse;
+	int m_LongueurDuSerpentCourant;
+	char m_Direction;
 
-
-	Snake()
+	//Trois constructeur surchargés
+	Snake::Snake()
 	{
-		m_Position = new POINT[5]{ 0 };
-		m_IdexTete = 0;
+		m_Position = new Point[5];
+		m_LongueurDuSerpentCourant = 5;
+		m_IndexTete = 0;
 		m_Vitesse = 100;
-	}
-	Snake(int Vitesse) :Snake()
+		m_Direction = 'D';
+	} 
+	Snake::Snake(int Vitesse, int Longueur)
 	{
 		m_Vitesse = Vitesse;
+		m_Position = new Point[Longueur];
+		m_LongueurDuSerpentCourant = Longueur;
+		m_IndexTete = 0;
+		m_Direction = 'D';
 	}
-	Snake(int Vitesse, int Longueur)
+	Snake::Snake(int Longueur)
 	{
-		m_Position = new POINT[Longueur]{ 0 };
-		m_IdexTete = 0;
-		m_Vitesse = Vitesse;
-
+		m_Position = new Point[Longueur];
+		m_LongueurDuSerpentCourant = Longueur;
+		m_IndexTete = 0;
+		m_Vitesse = 100;
+		m_Direction = 'D';
 	}
 
+	//Les methodes utilisables:
+	//int Snake::LongueurDuSerpent();  //retourne la longueur du serpent (nombre de segments)
+	void Snake::Allonger(int Longueur); //ajoute n(Longueur) nombre de segments
+	bool Snake::Update(char DirectionDuDeplacement, Point WindowSize);
+	bool Snake::Update(Point WindowSize);
+	
 
 };
